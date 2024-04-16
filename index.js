ba13d250fc29f486bd33ad5d3655613db6ca36da
josephmb830 <josephmoran@duck.com>
2024-04-16 17:46:18 -0500

    sumar variables

---
+++ index.js
@@ -0,0 +1,5 @@
+const numero1 = Number(prompt("Ingrese primer número"));
+const numero2 = Number(prompt("Ingrese segundo número"));
+const suma =numero1 +numero2;
+const mensajeSuma = "El resultado de la suma es "+suma;
+alert(mensajeSuma)
