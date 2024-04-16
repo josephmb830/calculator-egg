22ff867b8f4f8137e593e6c2c717ed98db3652b8
josephmb830 <josephmoran@duck.com>
2024-04-16 18:07:35 -0500

    resultado final de operaciones

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const numero1 = Number(prompt("Ingrese primer número"));
 const numero2 = Number(prompt("Ingrese segundo número"));
-const suma =numero1 +numero2;
-const mensajeSuma = "El resultado de la suma es "+suma;
-alert(mensajeSuma)
+const suma = numero1 + numero2;
+const mensajeSuma = "El resultado de la suma es " + suma;
+alert(mensajeSuma);
+const promedio = suma / 2;
+const triple = promedio * 3;
+const resultadoMenosDiez = triple - 10;
+const mensajeFinal =
+  "El resultado final de las operaciones es " + resultadoMenosDiez;
+alert(mensajeFinal);
