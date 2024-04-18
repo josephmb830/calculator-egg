47ba33ca8cc413bea80e9715c1dfd62e80fac51a
josephmb830 <josephmoran@duck.com>
2024-04-17 20:24:10 -0500

    sumando varios números

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const numero1 = Number(prompt("Ingrese primer número"));
-const numero2 = Number(prompt("Ingrese segundo número"));
-const suma = numero1 + numero2;
-const mensajeSuma = "El resultado de la suma es " + suma;
-alert(mensajeSuma);
-const promedio = suma / 2;
-const triple = promedio * 3;
-const resultadoMenosDiez = triple - 10;
-const mensajeFinal =
-  "El resultado final de las operaciones es " + resultadoMenosDiez;
-alert(mensajeFinal);
+const cantidad = Number(prompt("¿Cuántos números desea sumar?"));
+let sumatoria = 0;
+
+for (let index = 1; index <= cantidad; index++) {
+  const numeroAsumar = Number(prompt("Ingrese un número a sumar"));
+  sumatoria = sumatoria + numeroAsumar;
+}
+
+const mensajeSuma = "El resultado de la suma es " + sumatoria;
+
+console.log(mensajeSuma);
+
+// const numero1 = Number(prompt("Ingrese primer número"));
+// const numero2 = Number(prompt("Ingrese segundo número"));
+// const suma = numero1 + numero2;
+// const mensajeSuma = "El resultado de la suma es " + suma;
+// alert(mensajeSuma);
+// const promedio = suma / 2;
+// const triple = promedio * 3;
+// const resultadoMenosDiez = triple - 10;
+// const mensajeFinal =
+//   "El resultado final de las operaciones es " + resultadoMenosDiez;
+// alert(mensajeFinal);
